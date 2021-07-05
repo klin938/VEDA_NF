41e8f75830260cc52ae8ed86c8b4e63da026cae5
derlin <d.lin@garvan.org.au>
2021-07-05 18:10:25 +1000

    added vd_check_survive_sge.sh

---
+++ vd_check_survive_sge.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+q="${1:-short.q}"
+survival_rate="${2:-0.50}"
+
+# num of disabled divided by all
+
+count_all="$(qstat -f -q $q | grep $q | wc -l)"
+count_disabled="$(qstat -f -q $q -qs d | grep $q | wc -l)"
+current_rate="$(echo "scale=2; $count_disabled/$count_all" | bc)"
+
+if (( $(echo "$current_rate <= $survival_rate" | bc -l) )); then
+	printf "OK [ All: $count_all Disabled: $count_disabled Rate: $current_rate ]\n"
+else
+	printf "BAD [ All: $count_all Disabled: $count_disabled Rate: $current_rate ]\n"
+fi
+
+
